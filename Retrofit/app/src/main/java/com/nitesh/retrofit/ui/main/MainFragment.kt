62b4c4f77afcd8a16a7ed16dd69eba29dc6f22dc
MasterMinds <nits14@users.noreply.github.com>
2022-06-03 12:33:45 +0530

    Update MainFragment.kt
    unused import

--- Retrofit/app/src/main/java/com/nitesh/retrofit/ui/main/MainFragment.kt
+++ Retrofit/app/src/main/java/com/nitesh/retrofit/ui/main/MainFragment.kt
@@ -1,14 +1,13 @@
 package com.nitesh.retrofit.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.nitesh.retrofit.adapter.MyAdapter
 import com.nitesh.retrofit.databinding.FragmentMainBinding
